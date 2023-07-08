07fd8bb68374138fd112fca03ff0238249d56428
Mohammed Juzer <mohammedjuzer10@gmail.com>
2023-07-08 13:54:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// import "./sass/base/_typography.scss";
+// import "./sass/base/_fonts.scss";
+import "./sass/style.scss";
+
+console.log("Started wassup");
---
+++ webpack.dev.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // hmr: process.env.NODE_ENV === "development",
+          },
+          // Translates CSS into CommonJS
+          "css-loader",
+          //   for adding cssnano,autoprefix and ruckcss
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "autoprefixer",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+
+      //   Loading images
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+
+      // Loading fonts
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      filename: "index.html",
+      template: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./css/style.css",
+    }),
+  ],
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+};
